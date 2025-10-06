bae8f98ac2e87a81bd5f1999062d710449eacff0
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-10-06 15:02:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -1,21 +1,20 @@
 // User function Template for Java
 class Solution {
     static boolean armstrongNumber(int n) {
-        int original = n;
+        int orgNum = n;
         int sum = 0;
         
-        while(n>0){
-            int reminder = n%10;
-            n /= 10;
-            int cube = reminder * reminder * reminder;
+        while (n != 0){
+            int rem = n % 10;
+            n = n /10;
+            int cube = rem * rem * rem;
             sum += cube;
         }
         
-        if (sum == original){
+        if (orgNum == sum) {
             return true;
-        }
-        else {
-        return false;
+        } else {
+            return false;
         }
     }
 }
