060cfa523c0b200a5174b80ba0896d360003f65f
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-10-25 12:28:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,6 +1,7 @@
-class Solution {
-    public int getSecondLargest(int[] arr) {
-        int n = arr.length;
+// TC -> O(N^2)
+
+/* 
+int n = arr.length;
         int largest = 0;
         int secondLargest = -1;
         
@@ -16,5 +17,32 @@ class Solution {
             }
         }
         return secondLargest;
+        
+*/
+
+
+// Optimal Solution
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        int secondLargest = secondLargestElement(arr , n);
+
+        return secondLargest;
     }
+    
+    public int secondLargestElement(int[] arr , int n){
+        int largest = arr[0];
+        int secondLargest = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            } else if ((arr[i] < largest) && (arr[i] > secondLargest)){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+    
 }
