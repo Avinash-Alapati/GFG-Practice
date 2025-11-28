15c21722810434190b60a1a45988db91a2ecb225
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-11-28 13:50:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,29 @@
+// User function Template for Java
+
+class Solution {
+    static void rotateArr(int arr[], int d) {
+        // Bruteforce -> Copy kth elem --> Shift rem elem --> put it back !!
+        // BruteForce - TC -> O(d) + O(n-d) + O(d) = O(n + d);
+        // Extra Space SC -> O(d);
+        int n = arr.length;
+        d = d % n;
+        int[] temp = new int[d];
+        
+        // Copying the Kth elements to be Rotated
+        for(int i = 0; i < d; i++){
+            temp[i] = arr[i];
+        }
+        
+        // Shifting the remaining elements to left
+        for (int i = d; i < n; i++){
+            arr[i - d] = arr[i];
+        }
+        
+        // put the values in temp array back;
+        for (int i = 0; i < d; i++){
+            arr[n - d + i] = temp[i];
+        }
+        
+        
+    }
+}
