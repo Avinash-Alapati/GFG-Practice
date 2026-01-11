85e40d84cbbe251004c29eb637fb0c5ae7abc25b
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2026-01-11 22:04:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,9 +1,9 @@
-// TC -> O(N^2)
-
-/* 
-int n = arr.length;
-        int largest = 0;
-        int secondLargest = -1;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
         
         for (int i = 0; i < n; i++){
             if (arr[i] > largest) {
@@ -12,37 +12,10 @@ int n = arr.length;
         }
         
         for (int i = 0; i < n; i++){
-            if ((arr[i] > secondLargest) && (arr[i] != largest)){
-                secondLargest = arr[i];
-            }
-        }
-        return secondLargest;
-        
-*/
-
-
-// Optimal Solution
-class Solution {
-    public int getSecondLargest(int[] arr) {
-        int n = arr.length;
-        int secondLargest = secondLargestElement(arr , n);
-
-        return secondLargest;
-    }
-    
-    public int secondLargestElement(int[] arr , int n){
-        int largest = arr[0];
-        int secondLargest = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > largest){
-                secondLargest = largest;
-                largest = arr[i];
-            } else if ((arr[i] < largest) && (arr[i] > secondLargest)){
+            if(arr[i] > secondLargest && arr[i] < largest ){
                 secondLargest = arr[i];
             }
         }
         return secondLargest;
     }
-    
 }
