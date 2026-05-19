9e38798a1ffa1a1f3f99f35201d358613724a493
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2026-05-19 21:03:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -2,46 +2,20 @@
 
 class Solution {
     public int longestSubarray(int[] arr, int k) {
-    //  return bruteforceSolution(arr , k);
-     return betterSolution(arr , k);
-    }
-    
-    public int bruteforceSolution(int[] arr , int val){
-        int longest = 0;
-        int n = arr.length;
-        for(int i = 0; i < n; i++){
-            for (int j = i; j < n; j++){
-                int sum = 0;
-                for (int k = i; k < j; k++){
-                    sum += arr[k];
-                    
-                    if (sum == val) longest = Math.max(longest , j - i + 1);
-                }
+        // code here
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,-1);
+        int sum=0,max=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(map.containsKey(sum-k)){
+                int temp=i-map.get(sum-k);
+                max=Math.max(max,temp);
             }
-        }
-        return longest;
-    }
-    
-    public int betterSolution(int arr[] , int k){
-        HashMap<Integer , Integer> prefSumMap = new HashMap<>();
-        
-        int sum = 0;
-        int maxLen = 0;
-        for (int i = 0; i < arr.length; i++){
-            sum += arr[i];
-            if(sum == k) {
-                maxLen = Math.max(maxLen , i + 1);
-            }
-            int rem = sum - k;
+            if(!map.containsKey(sum))
+                map.put(sum,i);
             
-            if (prefSumMap.containsKey(rem)){
-                int len = i - prefSumMap.get(rem);
-                maxLen = Math.max(maxLen , len);
-            }
-            if (!prefSumMap.containsKey(sum)) {
-                prefSumMap.put(sum, i);
-            }
         }
-        return maxLen;
+        return max;
     }
 }
