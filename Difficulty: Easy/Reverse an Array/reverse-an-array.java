e94705d29f55e1e805ee132e5faed3c6f3d10cdf
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2026-06-04 13:46:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.java
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.java
@@ -1,33 +1,33 @@
 class Solution {
-    public void recursiveReverse(int i , int[] arr , int n){
-        // Base Case
-        if (i >= n/2){
+    public void reverseArray(int arr[]) {
+        // swapArr(arr);
+        int left = 0; 
+        int right = arr.length - 1;
+        recFun(arr, left, right);
+    }
+    
+    public void recFun(int[] arr, int left, int right){
+        if(left >= right) {
             return;
         }
-        
-        // Swapping
-        int temp = arr[i];
-        arr[i] = arr[n - i - 1];
-        arr[n - i - 1] = temp;
-        
-        // Recursion Call
-        recursiveReverse(i + 1 , arr , n);
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        recFun(arr, left + 1, right - 1);
     }
     
-    public void reverseArray(int arr[]) {
-        int n = arr.length;
-        recursiveReverse(0 , arr , n);
-    }
-    
-    // public int[] swap(int[] arr , int start , int end){
-    //     while (start < end) {
-    //         int temp = arr[start];
-    //         arr[start] = arr[end];
-    //         arr[end] = temp;
+    public int[] swapArr(int arr[]) {
+        int left = 0; 
+        int right = arr.length - 1;
+        
+        while( left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             
-    //         start++;
-    //         end--;
-    //     }
-    //     return arr;
-    // }
+            left++;
+            right--;
+        }
+        return arr;
+    }
 }
